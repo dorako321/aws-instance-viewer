e0f6fbcfbffb3a648bffc58206600e65ba16d0f5
YAMADA Daijiro <yamad@daiji.ro>
2016-06-29 00:03:26 +0900

    delete comment

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -5,14 +5,11 @@ angular.module('myApp', [])
                 method: 'GET',
                 url: '/ips/api/v1/ec2-list'
             }).success(function (data, status, headers, config) {
-                
                 _.each(data.Reservations, function (element, index, array) {
-                    //console.log(element.Instances[0]);
                     _.each(element.Instances[0].Tags, function (elem, idx, arr) {
                         var key = elem['Key'];
                         var value = elem['Value'];
                         data.Reservations[index].Instances[0].Tags[key] = value;
-                        //console.log(elem);
                     });
                 });
                 $scope.ec2Results = data.Reservations;
